31603b2bbd45671d9fb5fe8bb44712434ae1ac68
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-15 19:22:34 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -12,11 +12,3 @@ def hello_t(array)
     puts "Hey! No block was given!"
   end
 end
-
-# call your method here!
-
-["Tim", "Tom", "Jim"].each do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
